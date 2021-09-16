56ae0adb18aa3237dfd3d8de9a3250f72ef85afb
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-09-16 23:00:31 +0530

    committ

--- app.js
+++
@@ -1,7 +0,0 @@
-var btnTransalte = document.querySelector('#btn-transalte');
-var txtInput = document.querySelector('#txt-input')
-var outTxtDiv = document.querySelector('#out-Txt');
-btnTransalte.addEventListener("click", function clickEventHandler() {
-    outTxtDiv.innerHTML = txtInput.value;
-})
-
