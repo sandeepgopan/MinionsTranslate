8168ec70c602ee5b89def8a2471d7381d5fd9350
Sandeep Gopan <sandeepgopan111@gmail.com>
2021-09-18 00:46:10 +0530

    Api

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var btnTransalte = document.querySelector('#btn-transalte');
 var txtInput = document.querySelector('#txt-input')
 var outTxtDiv = document.querySelector('#out-Txt');
-btnTransalte.addEventListener("click", function clickEventHandler() {
-    outTxtDiv.innerHTML = txtInput.value;
-})
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+ function getTranslationURL(text){
+     return serverURL + "?" + "text=" + text 
+ }
+ function errorHandler(error){
+     console.log("error occured", error);
+     alert("error");
+ }
+function clickHandler(){
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json => {
+var translatedText = json.contents.translated;
+outTxtDiv.innerHTML = translatedText;
+    })
+    .catch(errorHandler);
+}
+ btnTransalte.addEventListener("click", clickHandler)
